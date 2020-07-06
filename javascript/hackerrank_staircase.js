2cfbc9386e67326434870c304a6394458e78356d
MisterHuh <jaehuh86@yahoo.com>
2020-07-05 17:27:51 -0700

    staircase solved

--- javascript/hackerrank_staircase.js
+++ javascript/hackerrank_staircase.js
@@ -7,49 +7,53 @@ let inputString = '';
 let currentLine = 0;
 
 process.stdin.on('data', inputStdin => {
-  inputString += inputStdin;
+    inputString += inputStdin;
 });
 
 process.stdin.on('end', _ => {
-  inputString = inputString.replace(/\s*$/, '')
-    .split('\n')
-    .map(str => str.replace(/\s*$/, ''));
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
 
-  main();
+    main();
 });
 
 function readLine() {
-  return inputString[currentLine++];
+    return inputString[currentLine++];
 }
 
 // Complete the staircase function below.
 function staircase(n) {
-  ;
 
-  let length = parseInt(n);
-  let sym = "#";
+    let length = parseInt(n);
+    let sym = "#";
+    let space = " ";
 
-  for (let index = 0; index < length; index++) {
+    for (let index = 1; index <= length; index++) {
+        let nextRow = '';
 
-    if (index === 0) {
-      console.log("     " + sym);
-    } else {
-      let nextRow = "1";
-      for (let inner = 1; inner <= index; inner++) {
-        nextRow = nextRow.concat(inner);
-        // console.log(nextRow);
-      };
-      // nextRow = nextRow.concat(sym);
-      console.log(nextRow);
+        if (index === 1) {
+            let position = length - index;
+            for (let i = 0; i < position; i++) {
+                nextRow += space;
+            }
+            nextRow += sym;
+            console.log(nextRow);
+        } else {
+            let position = length - index;
+            for (let i = 0; i < position; i++) {
+                nextRow += space;
+            };
+            for (let i2 = 0; i2 < index; i2++) {
+                nextRow += sym;
+            }
+            console.log(nextRow);
+        }
     }
-
-  }
-
-
 }
 
 function main() {
-  const n = parseInt(readLine(), 10);
+    const n = parseInt(readLine(), 10);
 
-  staircase(n);
+    staircase(n);
 }
